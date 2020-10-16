e17166264ba279b195b865f3ae7b0a9b3e959f84
makmakkerti <makmakkerti007@gmail.com>
2020-10-16 18:40:15 +0300

    App v1 finished

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,60 @@
 const weatherForm = document.querySelector('#searchForm');
 const search = document.querySelector('#search');
+search.addEventListener('click', () => {
+  search.setAttribute('placeholder', '');
+  search.value = '';
+});
+
+const textMessage = document.querySelector('#location');
+const widget = document.querySelector('.widget');
+const wIcon = document.querySelector('.weatherIcon');
+const wDate = document.querySelector('.date');
+const wTemperature = document.querySelector('.temperature');
+const wCondition = document.querySelector('.weatherCondition');
+const wPlace = document.querySelector('.place');
+
+const wFeelslike = document.querySelector('#feelslike span');
+const wPressure  = document.querySelector('#pressure span');
+const wHumidity  = document.querySelector('#humidity span');
+const wWindspeed = document.querySelector('#windspeed span');
+const wUvindex   = document.querySelector('#uvindex span');
+const wCloudcover = document.querySelector('#cloudcover span');
+
 const errorP = document.querySelector('#error');
-const locationP = document.querySelector('#location');
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  locationP.textContent = 'Loading...';
-  errorP.textContent = '';
+  widget.classList.add('hidden');
+  textMessage.textContent = 'Loading...';
 
   fetch(`http://localhost:3000/weather?address=${search.value})`)
     .then((response) => {
       response.json()
         .then((data) => {
           if (data.err) {
-            locationP.textContent = '';
-            errorP.textContent = data.err;
+            textMessage.textContent = data.err;
           } else {
-            const { country, region, name } = data.location;
+            textMessage.textContent = '';
+
+            const { country, name } = data.location;
             const { temperature, weather_icons,
               weather_descriptions, wind_speed,
-              pressure, humidity, feelslike } = data.current;
+              pressure, humidity, feelslike, uv_index, cloudcover } = data.current;
 
-            locationP.textContent = `Location: ${country}, ${region}, ${name} | Temperature: ${temperature}`;
-            // locationP.textContent = JSON.stringify(data);
+            wIcon.innerHTML = `<img src="${weather_icons[0]}" />`;
+            wTemperature.innerHTML = `<span>${temperature}°</span>`;
+            wCondition.textContent = weather_descriptions[0];
+            wPlace.textContent = `${country}, ${name}`;
+
+            wFeelslike.textContent = feelslike
+            wPressure.textContent = pressure;
+            wHumidity.textContent = humidity;
+            wWindspeed.textContent = wind_speed;
+            wUvindex.textContent = uv_index;
+            wCloudcover.textContent = cloudcover;
+            
+            widget.classList.remove('hidden');
           }
         })
     })
